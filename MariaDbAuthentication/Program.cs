f39df60b19003755419178f176f3e2a071559115
Su-Ting Chen <wendy10935@hotmail.com>
2018-12-26 23:56:18 +0800

    commit solution

---
+++ MariaDbAuthentication/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace MariaDbAuthentication
+{
+    public class Program
+    {
+        private static string _environmentName;
+
+        public static void Main(string[] args)
+        {
+            
+
+            //var configuration = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddJsonFile("appsettings.json")
+            //    .AddJsonFile($"appsettings.{_environmentName}.json", optional: true, reloadOnChange: true)
+            //    .Build();
+
+            //Log.Logger = new LoggerConfiguration()
+            //    .ReadFrom.Configuration(configuration)
+            //    .CreateLogger();
+
+            var currentEnv = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{currentEnv}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            //Configure logger
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+            var webHost = BuildWebHost(args);
+
+            webHost.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog((ctx, cfg) => cfg.ReadFrom.Configuration(ctx.Configuration))
+                .Build();
+    }
+}
